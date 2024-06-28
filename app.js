bf9c47b3457bbc125a3f9be54c978d4d81eca368
Sudipta Kumar Mondal <sudipta@codeclouds.in>
2024-06-28 19:08:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,110 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+const getAllTours = (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+};
+const getTour = (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour: tour,
+    },
+  });
+};
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+};
+
+const updateTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  const newTour = Object.assign(tour, req.body);
+  tours.push(newTour);
+
+  res.status(202).json({
+    status: "success",
+    data: {
+      tour: req.body,
+    },
+  });
+};
+
+const deteleTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+
+  const index = tours.indexOf(tour);
+  tours.splice(index, 1);
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
+
+app.get("/api/v1/tours", getAllTours);
+app.get("/api/v1/tours/:id", getTour);
+app.post("/api/v1/tours", createTour);
+app.patch("/api/v1/tours/:id", updateTour);
+app.delete("/api/v1/tours/:id", deteleTour);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
