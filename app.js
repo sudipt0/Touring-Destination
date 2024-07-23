d2488dc63c3f1d22af85b0a9e0652ae8c493e230
Sudipta Kumar Mondal <sudipta@codeclouds.in>
2024-07-23 19:03:56 +0530

    update cors

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -33,6 +34,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 /* Start: Global Middleware */
+// Implement CORS
+app.use(cors()); // Access-Control-Allow-Origin *
+
+app.options('*', cors());
 
 // Set security HTTP headers
 app.use(helmet());
